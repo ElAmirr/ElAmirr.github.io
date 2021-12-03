3b3618f81d5fc9a7cb529a5d668c109c8b2f8b21
ElAmirr <othmaniamir41@gmail.com>
2021-12-03 10:26:10 +0100

    add nav bar mobile version

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function nightFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+// small divice nav
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+  console.log('hello');
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
